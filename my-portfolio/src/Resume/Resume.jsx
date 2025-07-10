08a74b2c97798b99a2420d151eacef0c0be838fb
jithesh <jitheshacharya48@gmail.com>
2025-07-10 13:49:23 +0530

    Updated Resume section and added new resume files

--- my-portfolio/src/Resume/Resume.jsx
+++ my-portfolio/src/Resume/Resume.jsx
@@ -4,10 +4,10 @@ const Resume = () => {
   return (
     <div className='flex justify-items-center gap-5  ' >
         <div>
-        Download : <button className='bg-pink-400 h-[40px] w-[150px]  rounded '><a  href='/Jithesh.pdf'download>Download resume</a></button>
+        Download : <button className='bg-pink-400 h-[40px] w-[150px]  rounded '><a  href='/Jithesh1.pdf'download>Download resume</a></button>
         </div>
         <div>
-        View : <button className='bg-red-600 h-[40px] w-[150px]  rounded '><a  href='/Jithesh.pdf'>View resume</a></button>
+        View : <button className='bg-red-600 h-[40px] w-[150px]  rounded '><a  href='/Jithesh1.pdf'>View resume</a></button>
        </div>
     </div>
   )
